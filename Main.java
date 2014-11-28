dfe5dcae873df37bbd461c493b1e2ac6868b6149
Raffaele Montella <raffaele.montella@uniparthenope.it>
2014-11-28 09:56:44 +0100

    Added some methods

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
+  public Main(String[] args) {
+    System.out.println("Constructor");
+  }
+
+  public void run() {
+    System.out.println("Hello workd!");
+  }
+
   public static void main(String[] args) {
     System.out.println("This is the Main!");
+
+    Main main=new Main(args);
+    main.run();
   }
 
 }
